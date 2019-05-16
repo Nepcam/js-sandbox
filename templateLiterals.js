bb33d57d8e153eb6b4111a0c441e1f4e9a44f58e
Nepcam <cameron@aux.network>
2019-05-16 21:37:52 +1200

    template literals

--- templateLiterals.js
+++ templateLiterals.js
@@ -8,9 +8,27 @@ let html;
 html = '<ul><li>Name: ' + name + '</li><li>Age: ' + age + ' </li><li>Job '+ job + ' </li><li>City: ' + city +' </li></ul>';
 
 html = '<ul>' +
-        '<li>Name: ' + name + '</li> ' +
-        '<li>Age: ' + age + '</li> ' +
-        '<li>Job: ' + job + '</li> ' +
-        '<li>City: ' + city + '</li> ';
+            '<li>Name: ' + name + '</li> ' +
+            '<li>Age: ' + age + '</li> ' +
+            '<li>Job: ' + job + '</li> ' +
+            '<li>City: ' + city + '</li> '
+        '</ul>';
+
+function hello() {
+    return 'kia ora bro!!'
+};
+
+// With template strings
+html = `
+    <ul>
+        <li>Name: ${name}</li>
+        <li>Age: ${age}</li>
+        <li>Job: ${job}</li>
+        <li>City: ${city}</li>
+        <li>${10 * 10}</li>
+        <li>${hello()}</li>
+        <li>${age > 40 ? 'Over 40' : 'Under 40'}</li>
+    </ul>
+`;
 
 document.body.innerHTML = html;
