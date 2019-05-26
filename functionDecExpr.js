f34006db8a25e8383657ad700d4cff9eb1b90423
Nepcam <cameron@aux.network>
2019-05-26 14:40:22 +1200

    functions declarations & expressions

---
+++ functionDecExpr.js
@@ -0,0 +1,8 @@
+// FUNCTION DECLARATION
+
+function greet(firstName, lastName){
+   // console.log('Hello');
+   return 'Hello ' + firstName + ' ' + lastName;
+}
+
+console.log(greet('Cam', 'Nepe'));
